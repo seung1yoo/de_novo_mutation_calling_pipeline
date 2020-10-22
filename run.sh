d7dc43e5e8dbdc0870d309ea6a9c59d41cd7f0db
Seung-il Yoo <seung1.yoo@gmail.com>
2020-10-22 17:26:15 +0900

    add run.sh

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+snakemake \
+    -j 150 \
+    -s pipeline.snakefile \
+    -p \
+    --local-cores 8 \
+    --latency-wait=90 \
+    --max-jobs-per-second 5 \
+    --notemp \
+    --keep-target-files \
+    --keep-going \
+    --cluster-config cluster.json \
+    --cluster "qsub -V -o {config[workdir]}/{cluster.output} -e {config[workdir]}/{cluster.error} -pe smp {cluster.threads} -N {cluster.jobName} -S /bin/bash -q all.q"
